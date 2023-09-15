7ed285fb353c83213717ff9b8bff8fcfdfcdc401
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-09-15 17:40:58 +0900

    terraform/udemy-iac-with-terraform APサーバーのIAMロール追加

--- terraform/udemy-iac-with-terraform/iam.tf
+++ terraform/udemy-iac-with-terraform/iam.tf
@@ -11,3 +11,11 @@ data "aws_iam_policy_document" "ec2_assume_role" {
     }
   }
 }
+
+# ------------------------------
+# IAM Role
+# ------------------------------
+resource "aws_iam_role" "app_iam_role" {
+  name               = "${var.project}-${var.environment}-app-iam-role"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
