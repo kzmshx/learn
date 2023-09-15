6b3fe31d0038c99763a7e0f6726ab7008bf5c5f7
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-09-15 17:49:07 +0900

    terraform/udemy-iac-with-terraform IAMロールにポリシーをアタッチ

--- terraform/udemy-iac-with-terraform/iam.tf
+++ terraform/udemy-iac-with-terraform/iam.tf
@@ -19,3 +19,30 @@ resource "aws_iam_role" "app_iam_role" {
   name               = "${var.project}-${var.environment}-app-iam-role"
   assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
 }
+
+# ------------------------------
+# IAM Policy Attachment
+# ------------------------------
+resource "aws_iam_policy_attachment" "app_iam_role_ec2_readonly" {
+  name       = "${var.project}-${var.environment}-app-iam-role-ec2-read-only"
+  roles      = [aws_iam_role.app_iam_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
+}
+
+resource "aws_iam_policy_attachment" "app_iam_role_ssm_managed" {
+  name       = "${var.project}-${var.environment}-app-iam-role-ssm-managed"
+  roles      = [aws_iam_role.app_iam_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_policy_attachment" "app_iam_role_ssm_readonly" {
+  name       = "${var.project}-${var.environment}-app-iam-role-ssm-read-only"
+  roles      = [aws_iam_role.app_iam_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
+}
+
+resource "aws_iam_policy_attachment" "app_iam_role_s3_readonly" {
+  name       = "${var.project}-${var.environment}-app-iam-role-s3-read-only"
+  roles      = [aws_iam_role.app_iam_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
