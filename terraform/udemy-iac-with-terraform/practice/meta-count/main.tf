a6ab44429c0c13a355a1f5cc1438cec6bf6af645
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-09-16 10:39:27 +0900

    terraform/udemy-iac-with-terraform countメタ引数の練習

---
+++ terraform/udemy-iac-with-terraform/practice/meta-count/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "kzmshx_aws_test-terraform"
+  region  = "ap-northeast-1"
+}
+
+resource "aws_iam_user" "user" {
+  count = 2
+
+  name = "testuser-${count.index}"
+}
