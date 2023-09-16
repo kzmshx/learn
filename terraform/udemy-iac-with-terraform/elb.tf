e5aae1f04bce08f8efd273229b749ba6ff39d99f
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-09-16 11:30:05 +0900

    terraform/udemy-iac-with-terraform ALB Target Group 作成

--- terraform/udemy-iac-with-terraform/elb.tf
+++ terraform/udemy-iac-with-terraform/elb.tf
@@ -8,3 +8,24 @@ resource "aws_lb" "alb" {
   security_groups    = [aws_security_group.web_sg.id]
   subnets            = [aws_subnet.public_subnet_1a.id, aws_subnet.public_subnet_1c.id]
 }
+
+# ------------------------------
+# ALB Target Group
+# ------------------------------
+resource "aws_lb_target_group" "alb_target_group" {
+  name     = "${var.project}-${var.environment}-app-alb-tg"
+  port     = 3000
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-app-alb-tg"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+resource "aws_alb_target_group_attachment" "instance" {
+  target_group_arn = aws_lb_target_group.alb_target_group.arn
+  target_id        = aws_instance.app_server.id
+}
