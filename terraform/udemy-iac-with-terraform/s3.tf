25ca13b25ba6c1c244a6fa0cfbabc7ada7a4149d
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-09-17 10:16:53 +0900

    terraform/udemy-iac-with-terraform ソースコード配信用のS3バケット作成

--- terraform/udemy-iac-with-terraform/s3.tf
+++ terraform/udemy-iac-with-terraform/s3.tf
@@ -57,3 +57,55 @@ resource "aws_s3_bucket_policy" "s3_static_bucket" {
     aws_s3_bucket_public_access_block.s3_static_bucket
   ]
 }
+
+# ------------------------------
+# S3 Deploy Bucket
+# ------------------------------
+resource "aws_s3_bucket" "s3_deploy_bucket" {
+  bucket = "${var.project}-${var.environment}-deploy-bucket-${random_string.s3_unique_key.result}"
+}
+
+resource "aws_s3_bucket_versioning" "s3_deploy_bucket" {
+  bucket = aws_s3_bucket.s3_deploy_bucket.id
+  versioning_configuration {
+    status = "Disabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3_deploy_bucket" {
+  bucket = aws_s3_bucket.s3_deploy_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_deploy_bucket" {
+  bucket                  = aws_s3_bucket.s3_deploy_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+data "aws_iam_policy_document" "allow_app_role_access" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.s3_deploy_bucket.arn}/*"]
+    principals {
+      type        = "AWS"
+      identifiers = [aws_iam_role.app_iam_role.arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "s3_deploy_bucket" {
+  bucket = aws_s3_bucket.s3_deploy_bucket.id
+  policy = data.aws_iam_policy_document.allow_app_role_access.json
+
+  depends_on = [
+    aws_s3_bucket_public_access_block.s3_deploy_bucket
+  ]
+}
