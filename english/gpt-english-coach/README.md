bd75e6df19801e1119503903c17d07421966c75f
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-09-18 01:17:12 +0900

    english/gpt-english-coach 実装、ビルド

--- english/gpt-english-coach/README.md
+++ english/gpt-english-coach/README.md
@@ -1,17 +1,46 @@
-# gpt-english-coach
+# english/gpt-english-coach
 
-## Development
+## 機能
 
-To install dependencies:
+日本語文と英語文を入力すると、英作文のアドバイスを出力してくれる。
 
-```bash
-bun install
+```text
+$ gpt-english-coach
+日本語: 渋谷駅まで30分程度ウォーキングした
+英作文: I had a walking to Shibuya station approximately for 30 minutes.
+アドバイス:
+### 文法の間違い
+- had a walking → walked
+
+### 単語の意味やニュアンスの不適切な箇所
+- approximately → about
+
+### 修正例
+I walked to Shibuya station for about 30 minutes.
+
+### 他の表現例
+- I strolled to Shibuya station for approximately 30 minutes.
+- I took a leisurely walk to Shibuya station, which took about 30 minutes.
+- It took me around 30 minutes to walk to Shibuya station.
+
+### 長い日本語文
+渋谷駅まで30分程度ウォーキングした後、友達と会ってランチを食べました。その後、ショッピングをして帰りました。
+
+### 長い英作文
+After walking to Shibuya station for about 30 minutes, I met up with a friend and had lunch. Then, we went shopping before heading back.
 ```
 
-To run:
+## 開発
 
-```bash
-bun run index.ts
-```
-
-This project was created using `bun init` in bun v1.0.2. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
+- インストール
+  ```shell
+  bun install
+  ```
+- 実行
+  ```shell
+  bun run index.ts
+  ```
+- ビルド
+  ```shell
+  bun run build
+  ```
--- english/gpt-english-coach/index.ts
+++ english/gpt-english-coach/index.ts
@@ -1,1 +1,82 @@
-console.log("Hello via Bun!");
+import { Command } from "commander";
+import figlet from "figlet";
+import OpenAI from "openai";
+import * as readline from "readline/promises";
+
+const HELP_HEADER = figlet.textSync("GPT English Coach") + "\n";
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+const prompt = (japanese: string, english: string): string => `\\
+以下は生徒が作成した日本語文とその英作文だ。条件に従ってアドバイスを示せ。
+
+// 条件
+- 英作文の文法の間違いを指摘せよ
+- 英作文の単語の意味やニュアンスの不適切な箇所を指摘せよ
+- アドバイスに沿って英作文を修正せよ
+- 日本語文に対する英作文の他の表現例を3個生成せよ
+- 日本語文から長い文章を生成（2文追加）し、それに対する英作文を生成せよ
+
+// 日本語
+${japanese}
+
+// 英作文
+${english}
+
+// 出力形式
+### 文法の間違い
+- [文法の間違い1]
+- ...
+### 単語の意味やニュアンスの不適切な箇所
+- [単語の意味やニュアンスの不適切な箇所1]
+- ...
+### 修正例
+[修正例]
+### 他の表現例
+- [他の表現例1]
+- [他の表現例2]
+- [他の表現例3]
+### 長い日本語文
+[日本語文]
+### 長い英作文
+[英作文]
+
+// 出力`;
+
+const run = async (japanese?: string, english?: string): Promise<void> => {
+  if (!japanese || !english) {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    japanese = await rl.question("日本語: ");
+    english = await rl.question("英作文: ");
+    rl.close();
+  }
+
+  const completion = await openai.chat.completions.create({
+    messages: [
+      { role: "system", content: "あなたは有能な英語コーチだ。" },
+      { role: "user", content: prompt(japanese, english) },
+    ],
+    model: "gpt-3.5-turbo",
+    temperature: 0.4,
+  });
+
+  console.log("アドバイス:");
+  console.log(completion.choices[0].message.content);
+};
+
+const main = async (): Promise<void> => {
+  await new Command("gpt-english-coach")
+    .version("0.1.0")
+    .addHelpText("beforeAll", HELP_HEADER)
+    .argument("[japanese]", "日本語文")
+    .argument("[english]", "英作文")
+    .action(run)
+    .showHelpAfterError(true)
+    .showSuggestionAfterError(true)
+    .parseAsync(process.argv);
+};
+
+main();
