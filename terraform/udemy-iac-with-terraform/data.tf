4d7cff3978230cffc06cf9a0cd04c059d481ad06
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-09-17 21:24:20 +0900

    terraform/udemy-iac-with-terraform 作成済みのAMIを使う

--- terraform/udemy-iac-with-terraform/data.tf
+++ terraform/udemy-iac-with-terraform/data.tf
@@ -6,18 +6,23 @@ data "aws_ami" "app" {
   most_recent = true
   owners      = ["self", "amazon"]
 
+  #  filter {
+  #    name   = "name"
+  #    values = ["amzn2-ami-kernel-5.10-hvm-2.0.*.0-x86_64-gp2"]
+  #  }
+  #
+  #  filter {
+  #    name   = "root-device-type"
+  #    values = ["ebs"]
+  #  }
+  #
+  #  filter {
+  #    name   = "virtualization-type"
+  #    values = ["hvm"]
+  #  }
+
   filter {
     name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-2.0.*.0-x86_64-gp2"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+    values = ["tastylog-app-ami-2"]
   }
 }
