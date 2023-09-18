7fd1d369a051f619a4c4fc1430cf382e069c9f5c
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-09-18 16:46:16 +0900

    terraform/udemy-iac-with-terraform IAMユーザー・グループ作成

---
+++ terraform/udemy-iac-with-terraform/iam-user/main.tf
@@ -0,0 +1,42 @@
+# ------------------------------
+# Terraform configuration
+# ------------------------------
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "tastylog-tfstate-bucket-kzmshx"
+    key     = "tastylog-user.tfstate"
+    region  = "ap-northeast-1"
+    profile = "kzmshx_aws_test-terraform"
+  }
+}
+
+# ------------------------------
+# Provider
+# ------------------------------
+provider "aws" {
+  profile = "kzmshx_aws_test-terraform"
+  region  = "ap-northeast-1"
+}
+
+# ------------------------------
+# Variables
+# ------------------------------
+variable "project" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "domain" {
+  type = string
+}
