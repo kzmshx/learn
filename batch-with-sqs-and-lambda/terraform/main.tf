e15780b5fdb6dd6db788075668c60c1154ceb14f
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-10-06 13:31:35 +0900

    batch-with-sqs-and-lambda main.tf 作成

---
+++ batch-with-sqs-and-lambda/terraform/main.tf
@@ -0,0 +1,22 @@
+# ------------------------------
+# Terraform Configuration
+# ------------------------------
+terraform {
+  required_version = "~> 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.19.0"
+    }
+  }
+
+  backend "s3" {}
+}
+
+# ------------------------------
+# Provider Configuration
+# ------------------------------
+provider "aws" {
+  region = var.region
+}
