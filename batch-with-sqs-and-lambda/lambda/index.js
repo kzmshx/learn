35ede04b0b440d2ab2388217eeff38c0ad1adcf3
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-10-08 01:46:53 +0900

    batch-with-sqs-and-lambda Add lambda function implementation

--- batch-with-sqs-and-lambda/lambda/index.js
+++ batch-with-sqs-and-lambda/lambda/index.js
@@ -1,8 +1,30 @@
-exports.handler = async (event) => {
-  console.log(event)
+import { S3, GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
+import sharp from "sharp";
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify('Hello, world!'),
-  }
-}
+const OUTPUT_BUCKET = process.env.OUTPUT_BUCKET;
+
+const s3 = new S3();
+
+export const handler = async (event) => {
+  const s3Event = JSON.parse(event.Records[0].body);
+  const bucket = s3Event.Records[0].s3.bucket.name;
+  const key = s3Event.Records[0].s3.object.key;
+
+  const { Body } = await s3.send(
+    new GetObjectCommand({
+      Bucket: bucket,
+      Key: key,
+    }),
+  );
+
+  const buf = Buffer.from(await Body.transformToByteArray());
+
+  await s3.send(
+    new PutObjectCommand({
+      Bucket: OUTPUT_BUCKET,
+      Key: key,
+      Body: await sharp(buf).resize(512, 512, { fit: "contain" }).toBuffer(),
+      ContentType: "image/jpeg",
+    }),
+  );
+};
