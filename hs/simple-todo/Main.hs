7d1868189bd74449926277d44df810894d079e15
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-12-03 17:36:27 +0900

    simple-todo Define actions by functions

--- hs/simple-todo/Main.hs
+++ hs/simple-todo/Main.hs
@@ -4,6 +4,15 @@ type Item = String
 
 type Items = [Item]
 
+-- addItem returns a new list with the item added to the end.
+addItem :: Item -> Items -> Items
+
+-- displayItems returns a string representation of the items.
+-- displayItems :: Items -> String
+
+-- removeItem returns a new list without the item at the given index, if not found returns an error message.
+-- removeItem :: Int -> Items -> Either String Items
+
 main :: IO ()
 main = do
   putStr "Name: "
