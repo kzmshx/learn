6fa5f2d016d3a9e9dd79af6109a3372488483952
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-12-03 22:56:25 +0900

    simple-todo Implement displayItems

--- hs/simple-todo/Main.hs
+++ hs/simple-todo/Main.hs
@@ -9,7 +9,12 @@ addItem :: Item -> Items -> Items
 addItem item items = item : items
 
 -- displayItems returns a string representation of the items.
--- displayItems :: Items -> String
+displayItems :: Items -> String
+displayItems items =
+  let displayItem index item = show index ++ " - " ++ item
+      reversedItems = reverse items
+      displayItemsList = zipWith displayItem [1 ..] reversedItems
+   in unlines displayItemsList
 
 -- removeItem returns a new list without the item at the given index, if not found returns an error message.
 -- removeItem :: Int -> Items -> Either String Items
